d08d4c042d3fc75cbfe2bba334d8f0208a7e0ad7
cjy161220011 <42716898+cjy161220011@users.noreply.github.com>
2019-07-14 14:27:16 +0800

    添加task5标签传播部分

--- test.py
+++ test.py
@@ -97,26 +97,131 @@ def turnPairs(file):
         values.append((key, line))
     return values
 
-'''sc = SparkContext(appName='test')
-file = sc.textFile("novels")
-#file = sc.wholeTextFiles("novels").flatMap(lambda x: turnPairs(x)).map(lambda x: split_line(x)).sortBy(lambda x: x[1], True).partitionBy(14, lambda x: x-1)
-splitNameFile = file.map(lambda line: split_line(line))
-splitNameFile.filter(lambda words: len(words) > 0).sortBy(lambda x: x, True, 1).saveAsTextFile("sss")'''
+
+init_label = -1
+
+def set_label():
+    global init_label
+    #src_name = line[0]
+    #dst_namelist = line[1]
+
+    init_label = init_label + 1
+
+    return init_label
+
+def send_label(item):
+    src_name = item[0]
+    dst_list = item[1][0]
+    label = item[1][1]
+    length = int(len(dst_list)/2)
+    result = []
+    for i in range(length):
+        dst_name = dst_list[i*2]
+        result.append(  (dst_name, (src_name, label) )  )
+
+    return result
+
+def reduce_label(x,y):
+    result = []
+    for item in x:
+        result.append(item)
+    for item in y:
+        result.append(item)
+    return result
+
+def max_label(item):
+    link_list = item[1][0]
+    label_list = item[1][1]
+    length = int(len(label_list)/2)
+
+    sum_list = []
+    for i in range(length):
+        label = label_list[2*i+1]
+        sum_list.append( (label, 0) )
+
+    for i in range(length):
+        name = link_list[2*i]
+        for j in range(length):
+            if label_list[2*j] == name:
+                label = label_list[2*j+1]
+                for k in sum_list:
+                    if k[0] == label:
+                        sum = k[1]+link_list[2*i+1]
+                        sum_list.remove(k)
+                        sum_list.append( (label, sum) )
+
+    new_label = ""
+    max_value = 0
+    for i in sum_list:
+        if i[1]>max_value:
+            max_value = i[1]
+            new_label = i[0]
+
+    return (item[0], new_label)
 
 
-sc = SparkContext(appName='test')
-file = sc.textFile("test-out")
-distinctNameFile = file.map(lambda line: line.strip("\t")).map(lambda line: line.split(" ")).map(lambda words: distinct_name(words)).filter(lambda words: len(words) > 0)
-turples = distinctNameFile.flatMap(lambda names: generateRelation(names)).filter(lambda turples: len(turples) > 0).reduceByKey(lambda a, b: a+b)
-network = turples.map(lambda relation: generateNet(relation)).reduceByKey(lambda a, b: mergeList(a, b)).map(lambda net: normalize(net))
-network.sortBy(lambda x: x, True, 1).saveAsTextFile("result1")
+def reduce_name(x, y):
+    names = []
+    for item in x:
+        names.append(item)
+    for item in y:
+        names.append(item)
 
-#PageRank
-time = 10
-lines = network
-links = lines.map(lambda line: (line[0], line[1]))
-ranks = links.mapValues(lambda x: 1.0)
-for i in range(time):
-    ranks = links.join(ranks).flatMap(split_links).reduceByKey(lambda x, y: x + y)
-ranks.sortBy(lambda x: x[1], False, 1).saveAsTextFile("result2")
+    return names
 
+
+if __name__=='__main__':
+    '''
+    sc = SparkContext(appName='test')
+    file = sc.textFile("novels")
+    #file = sc.wholeTextFiles("novels").flatMap(lambda x: turnPairs(x)).map(lambda x: split_line(x)).sortBy(lambda x: x[1], True).partitionBy(14, lambda x: x-1)
+    splitNameFile = file.map(lambda line: split_line(line))
+    splitNameFile.filter(lambda words: len(words) > 0).sortBy(lambda x: x, True, 1).saveAsTextFile("sss")
+    '''
+
+
+    sc = SparkContext(appName='test')
+    file = sc.textFile("task1-output.txt")
+    distinctNameFile = file.map(lambda line: line.strip("\t"))\
+                           .map(lambda line: line.split(" "))\
+                           .map(lambda words: distinct_name(words))\
+                           .filter(lambda words: len(words) > 0)
+
+
+
+    turples = distinctNameFile.flatMap(lambda names: generateRelation(names))\
+                              .filter(lambda turples: len(turples) > 0)\
+                              .reduceByKey(lambda a, b: a+b)
+    network = turples.map(lambda relation: generateNet(relation))\
+                     .reduceByKey(lambda a, b: mergeList(a, b))\
+                     .map(lambda net: normalize(net))
+    #network.sortBy(lambda x: x, True, 1).saveAsTextFile("result1")
+
+    #PageRank
+    time = 10
+    lines = network
+    links = lines.map(lambda line: (line[0], line[1]))
+    ranks = links.mapValues(lambda x: 1.0)
+    for i in range(time):
+        ranks = links.join(ranks)\
+                     .flatMap(split_links)\
+                     .reduceByKey(lambda x, y: x + y)
+
+    #ranks.sortBy(lambda x: x[1], False, 1).saveAsTextFile("result2")
+
+
+    #Label Passing
+    lines = network
+    ranks = lines.mapValues(lambda x: set_label())
+
+    for i in range(time):
+        print(len(ranks.map(lambda item: (item[1], [item[0]])).reduceByKey(lambda x, y: reduce_name(x, y)).collect()))
+        a = links.join(ranks)\
+                 .flatMap(send_label)\
+                 .reduceByKey(lambda x,y: reduce_label(x,y))
+        ranks = links.join(a).map(max_label)
+
+
+    ranks = ranks.map(lambda item: (item[1], [item[0]])).reduceByKey(lambda x,y: reduce_name(x, y) )
+    print(len(ranks.collect()))
+    ranks.sortBy(lambda x: x[0], False, 1).saveAsTextFile("result3")
